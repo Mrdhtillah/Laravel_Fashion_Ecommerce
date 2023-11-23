c4af9d72d2f1f77f4c2b4d5a82042a7583418076
Lithashl <90357502+Lithashl@users.noreply.github.com>
2023-11-23 11:13:26 +0700

    Add files via upload

--- app/Http/Controllers/MyController.php
+++ app/Http/Controllers/MyController.php
@@ -1,38 +1,56 @@
 <?php
 
+
 namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Storage;
+
+
+
+
 use Illuminate\Http\Request;
 
 class MyController extends Controller
 {
-    public function index()
-    {
+    public function index(){
         return view("admin.login");
     }
-
     public function store(Request $request)
     {
-        $credentials = $request->only('email', 'password');
+        // Validasi input
+        $validator = Validator::make($request->all(), [
+            'email' => 'required|string|max:255',
+            'password' => 'required|string|max:255',
+        ]);
 
-        if (Auth::attempt($credentials)) {
-            if (Auth::user()->role === 'admin') {
-                return redirect("/admin")->with('success', 'Logged in successfully.');
-            } else {
-                Auth::logout();
-                return redirect('/admin/login')->with('error', 'Not authorized as admin.');
-            }
-        } else {
-            return redirect('/admin/login')->with('error', 'Invalid credentials.');
+        if ($validator->fails()) {
+            return redirect('/admin/login')
+                ->withErrors($validator)
+                ->withInput();
         }
-    }
 
-    public function logout()
-    {
-        Auth::logout();
-        return redirect('/')->with('success', 'Logged out successfully.');
+        $email = $request -> input('email');
+        $password = $request -> input('password');
+        
+        $data = array(
+            'email'=>$email,
+            'password'=>$password);
+        $user = DB::select("select * from users where email = '".$data["email"]."' and role = 'admin'");
+        if($user==""){
+            echo "Email Not Found as Admin.<br/>";
+        } 
+        else{
+            if(Auth::attempt(['email' => $data['email'], 'password' => $data['password']]))
+            {
+                session()->put('email', $data['email']);
+                return redirect ("/admin");
+            }
+            else{
+                echo "Password incorrect.<br/>";
+            }
+        }
+       
     }
 }
